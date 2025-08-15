a24da24415879dbd4e5de59cbe78ef8648410b77
sontxps38973 <sontxps38973@gmail.com>
2025-08-15 12:30:16 +0700

    up

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+
+        // CORS Headers
+        $response->headers->set('Access-Control-Allow-Origin', 'https://bmbfunctionalfood.onrender.com');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, X-CSRF-TOKEN');
+        $response->headers->set('Access-Control-Allow-Credentials', 'true');
+        $response->headers->set('Access-Control-Max-Age', '86400');
+
+        // Handle preflight OPTIONS request
+        if ($request->isMethod('OPTIONS')) {
+            $response->setStatusCode(200);
+            $response->setContent('');
+        }
+
+        return $response;
+    }
+}
